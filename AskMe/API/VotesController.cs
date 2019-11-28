4f487107721a4b7a1895c59761ae547d49f6da4e
abhi <abrahmbhatt03@gmai.com>
2019-11-28 09:46:26 -0600

    Add project files.

---
+++ AskMe/API/VotesController.cs
@@ -0,0 +1,52 @@
+﻿using AskMe.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace AskMe.API
+{
+  [Authorize]
+  public class VotesController : ApiController
+  {
+    ApplicationDbContext _context = new ApplicationDbContext();
+
+    [HttpPost]
+    public IHttpActionResult UpVote(int id)
+    {
+      var user = _context.Users.Find(User.Identity.GetUserId());
+      var post = _context.Posts.Find(id);
+      var votes = post.Votes.ToList();
+      if (votes.FindAll(u => u.UserId == user.Id).Count == 0)
+      {
+        Votes vote = new Votes
+        {
+          VotedDateTime = DateTime.Now,
+          UserId = user.Id,
+          User = user,
+          Post = post,
+          PostId = id,
+        };
+        post.Votes.Add(vote);
+        _context.SaveChanges();
+        return Ok();
+      }
+      return BadRequest();
+    }
+
+    public IHttpActionResult DownVote(int id)
+    {
+      var user = _context.Users.Find(User.Identity.GetUserId());
+      var post = _context.Posts.Find(id);
+      var votes = post.Votes.ToList();
+      if (votes.FindAll(u => u.UserId == user.Id).Count == 1)
+      {
+        var voteInDb = _context.Votes.Where(v => v.PostId == id && v.UserId == user.Id).FirstOrDefault();
+        _context.Votes.Remove(voteInDb);
+        _context.SaveChanges();
+        return Ok();
+      }
+      return BadRequest();
+    }
+  }
+}
---
+++ AskMe/Models/ViewModel/QuestionDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AskMe.Models.ViewModel
+{
+    public class QuestionDetailViewModel
+    {
+        public QuestionDetailViewModel()
+        {
+            VoteCount = 0;
+        }
+        public Question Question { get; set; }
+        [Display(Name = "Add your answer.")]
+        public string AnswerContent { get; set; }
+        public string UserId { get; set; }
+        public int QuestionId { get; set; }
+        public int AnswerId { get; set; }
+        public int VoteCount { get; set; }
+    }
+}
